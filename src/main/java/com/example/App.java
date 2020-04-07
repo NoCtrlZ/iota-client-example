30338fcbc8f077850a21b3dedcd9cc458fdd13dd
NoCtrlZ <phantomofrotten@gmail.com>
2020-04-07 19:02:22 +0900

    setup spring boot

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,21 @@
-package com.example;
+package iota_example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class App {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring Boot!";
+    }
+
+    public static void main(String[] args) {
+      SpringApplication.run(App.class, args);
     }
 }
