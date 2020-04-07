114c041856cc1eaa5371cbd106492ce7b54b3347
NoCtrlZ <phantomofrotten@gmail.com>
2020-04-07 17:55:22 +0900

    success connect to iota node

--- src/main/java/com/example/iota/ConnectToNode.java
+++ src/main/java/com/example/iota/ConnectToNode.java
@@ -2,8 +2,9 @@ package com.iota;
 
 import org.iota.jota.IotaAPI;
 import org.iota.jota.dto.response.GetNodeInfoResponse;
+import org.iota.jota.error.ArgumentException;
 
-class ConnectToNode {
+public class ConnectToNode {
 public static void main(String[] args) throws ArgumentException {
 
         // Create a new instance of the API object
