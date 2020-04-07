2ded14055043fe05ea831b6b77d9a975fe770446
NoCtrlZ <phantomofrotten@gmail.com>
2020-04-07 19:28:19 +0900

    change new address method for spring boot

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,14 +3,14 @@ package iota_example;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 public class App {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Hello Spring Boot!";
     }
--- src/main/java/com/example/iota/NewAddress.java
+++ src/main/java/com/example/iota/NewAddress.java
@@ -6,7 +6,7 @@ import org.iota.jota.dto.response.GetNewAddressResponse;
 import org.iota.jota.error.ArgumentException;
 
 class NewAddress {
-    public static void main(String[] args) throws ArgumentException {
+    public static String main(String mySeed) throws ArgumentException {
         IotaAPI api = new IotaAPI.Builder()
         .protocol("https")
         .host("nodes.devnet.thetangle.org")
@@ -14,12 +14,14 @@ class NewAddress {
         .build();
 
         int securityLevel = 2;
-        String mySeed = "PUPTTSEITFEVEWCWBTSIZM9NKRGJEIMXTULBACGFRQK9IMGICLBKW9TTEVSDQMGWKBXPVCBMMCXWMNPDX";
         try {
             GetNewAddressResponse response = api.generateNewAddresses(new AddressRequest.Builder(mySeed, securityLevel).amount(1).checksum(true).build());
-            System.out.printf("Your address is %s", response.getAddresses());
+            String address = response.getAddresses().toString();
+            return address;
         } catch (ArgumentException e) {
             e.printStackTrace();
+            String error = "None";
+            return error;
         }
     }
 }
